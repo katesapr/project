b1c09f8b7b40d9e5ca2db97f5c4398d984d46858
KateSapr <katessapr@gmail.com>
2024-11-08 19:19:59 +0300

    progect

---
+++ script.js
@@ -0,0 +1,19 @@
+const catsDB = [
+    {
+        name: "",
+        age: 2,
+        img: ""
+    },
+    {
+        name: "",
+        age: 2,
+        img: ""
+    },
+    {
+        name: "",
+        age: 2,
+        img: ""
+    },
+]
+let name = document.getElementById("name").value
+let age = 
